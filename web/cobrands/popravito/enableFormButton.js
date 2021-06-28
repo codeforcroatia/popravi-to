c5ef13ab371eb6ca557332020b6560ac7aea074b
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2021-06-28 14:50:29 +0200

    enable form button error in event handling

--- web/cobrands/popravito/enableFormButton.js
+++ web/cobrands/popravito/enableFormButton.js
@@ -1,7 +1,7 @@
 const enableFormButton = function(){
     var formElements = document.getElementsByClassName("disabled-button")
     for (var i = 0; i < formElements.length; i++) {
-        formElements[i].addEventListener("change", function() {
+        formElements[i].addEventListener("click", function() {
             if (document.getElementById("username").value === "" && document.getElementById("password_sign_in").value === "") {
                 formElements[i].disabled = true
             } else {
