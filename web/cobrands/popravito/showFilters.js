d733b3f63fe6840fab66f9439d5258441be89aa3
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2021-07-22 14:00:13 +0200

    added show filters function

---
+++ web/cobrands/popravito/showFilters.js
@@ -0,0 +1,10 @@
+const toggleFilterModel = function(){
+    let filterButton = document.getElementById('filterButton')
+
+    const toggleFilter = () => {
+        document.querySelector('#filterModal').classList.toggle('visually-hidden')
+    }
+
+    filterButton.addEventListener('click', toggleFilter)
+    
+}()
