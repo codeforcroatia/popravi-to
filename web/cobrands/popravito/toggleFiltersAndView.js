3204fa513f625ee5c5ead6b6bda81eb92a0b4c54
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2021-07-23 15:52:07 +0200

    added active class for list and map view buttons

--- web/cobrands/popravito/toggleFiltersAndView.js
+++ web/cobrands/popravito/toggleFiltersAndView.js
@@ -10,11 +10,15 @@ const toggleFiltersAndView = function(){
     const toggleListView = () => {
         document.getElementById('reportsContainer').classList.remove('visually-hidden')
         document.getElementById('mapContainer').classList.add('visually-hidden')
+        listButton.classList.add('list-map-active')
+        mapButton.classList.remove('list-map-active')
     }
 
     const toggleMapView = () => {
         document.getElementById('reportsContainer').classList.add('visually-hidden')
         document.getElementById('mapContainer').classList.remove('visually-hidden')
+        mapButton.classList.add('list-map-active')
+        listButton.classList.remove('list-map-active')
     }
 
     filterButton.addEventListener('click', toggleFilter)
