2ddce28044877613724cef4a15ad9a9f9b0fd670
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2021-07-30 18:04:26 +0200

    changed selected class

--- web/cobrands/popravito/toggleUpdateForm.js
+++ web/cobrands/popravito/toggleUpdateForm.js
@@ -3,14 +3,14 @@ const toggleUpdateForm = function(){
           updateForm = document.getElementById('update_form')
     
     const openUpdateForm = () => {
-        const problemHeader = document.getElementsByClassName('problem-header')
+        const problemHeader = document.getElementsByClassName('clearfix')
         const updatesContainer = document.getElementsByClassName('updates-content')
         const rssContainer = document.getElementsByClassName('shadow-wrap')
 
         problemHeader.classList.add('visually-hidden')
         updatesContainer.classList.add('visually-hidden')
         rssContainer.classList.add('visually-hidden')
-        
+
         updateForm.classList.remove('visually-hidden')
     }
 
