e6e00a1558cfdabdb4d7dcef1076774f2cf052ed
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2021-08-14 14:47:04 +0200

    started creating toggle modal funcitn

--- web/cobrands/popravito/toggleInfoModals.js
+++ web/cobrands/popravito/toggleInfoModals.js
@@ -1,1 +1,18 @@
-alert('toggle')
+const toggleInfoModals = function(){
+    const modalButton = document.querySelectorAll('.details-button')
+    const modalButtonArray = Array.from(modalButton).entries();
+
+    const openInfoModal = () => {
+       /* for (let [index, modal] of modalButton) {
+            const toggleModal => () {
+              modals[index].classList.toggle("show-modal");
+            };
+            trigger.addEventListener("click", toggleModal);
+            closeButtons[index].addEventListener("click", toggleModal);
+        }
+       */
+      console.log(modalButton)
+    }
+
+    modalButton.addEventListener('click', openInfoModal)
+}()
