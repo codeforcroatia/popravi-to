2ed44896c09ea56e2b91109277980d02d03d9b4c
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2021-08-14 16:39:33 +0200

    debug info modal function

--- web/cobrands/popravito/toggleInfoModals.js
+++ web/cobrands/popravito/toggleInfoModals.js
@@ -1,15 +1,10 @@
 const toggleInfoModals = function(){
-    const modalButton = document.querySelectorAll('.details-button')
-        modalButtonArray = Array.from(modalButton).entries();
-        infoModals = document.querySelectorAll('.info-modal')
+    const modalButton = document.getElementsByClassName('details-button')
+        infoModals = document.getElementsByClassname('info-modal')
 
-    for (let [index, modal] of modalButtonArray){
-
-        const toggleModal = () => {
-            console.log(infoModals[index])
+    for (let [index, modal] of modalButton){
+        modal.addEventListener("click", function(){
             infoModals[index].classList.toggle('show-info-modal')
-        }
-
-        modal.addEventListener("click", toggleModal);
+        });
     }
 }()
