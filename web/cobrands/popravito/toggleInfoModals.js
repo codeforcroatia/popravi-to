bcb8f3ccb093e437a2c769c9b4002eda7825284c
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2021-08-15 14:58:39 +0200

    add close buttons

--- web/cobrands/popravito/toggleInfoModals.js
+++ web/cobrands/popravito/toggleInfoModals.js
@@ -2,11 +2,20 @@ const toggleInfoModals = function(){
     const modalButton = document.getElementsByClassName('details-button')
         infoModals = document.getElementsByClassName('info-container')
         modalTriggers = Array.from(modalButton).entries();
+        closeModalButton = document.getElementsByClassName('close-info-modal')
+        closeTriggers = Array.from(closeModalButton).entries()
 
     for (let [index, modal] of modalTriggers){
-        modal.addEventListener("click", function(){
+        modal.addEventListener('click', function(){
             infoModals[index].classList.toggle('show-info-modal')
             infoModals[index].classList.toggle('visually-hidden')
         });
+    }
+
+    for (let [index, button] of closeTriggers){
+        button.addEventListener('click', function(){
+            button.parentNode.parentNode.classList.toggle('show-info-modal')
+            button.parentNode.parentNode.classList.toggle('visually-hidden')
+        })
     }
 }()
